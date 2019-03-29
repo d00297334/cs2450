e50c976703b40da3d527da8a2f44f64b29a88fdb
Kathleen Cram <d00297334@dmail.dixie.edu>
2019-03-29 15:18:31 -0600

    added app

---
+++ client/app.js
@@ -0,0 +1,12 @@
+const app = new Vue ({
+    el: '#app',
+    data: {
+        message: 'test'
+    },
+    methods: {
+
+    },
+    created: function() {
+        console.log('page loaded/created');
+    }
+});
